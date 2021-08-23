03881f2b90e7a2969a5b3a9128aa31896647d1c3
superhan-dev <superhan.dev@gmail.com>
2021-08-23 21:37:39 +0900

    first commit

---
+++ spring-boot-boilerplate/src/test/java/com/superhan/springbootboilerplate/SpringBootBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superhan.springbootboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
